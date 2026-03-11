9ecf34b4a404c6c74baf2435a88eda54acc428e8
VorteX <151031855+Kingfinik98@users.noreply.github.com>
2026-03-11 18:30:19 +0700

    Update inject.sh

--- inject_ksu/inject.sh
+++ inject_ksu/inject.sh
@@ -15,6 +15,6 @@ fi
 # Perform replacement configuration to ManualHz
 sed -i 's/config HZ_1000/config HZ_1000/g' "$KCONFIG_HZ"
 sed -i 's/bool "1000 HZ"/bool "1000 HZ"/g' "$KCONFIG_HZ"
-sed -i 's/default 1000 if HZ_1000/default 500 if HZ_500/g' "$KCONFIG_HZ"
+sed -i 's/default 1000 if HZ_1000/default 1000 if HZ_1000/g' "$KCONFIG_HZ"
 
 echo " Successfully patched kernel for 1000Hz support."
