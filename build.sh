783762673636b6b47c449a1cdec4843bbd075c95
VorteX <151031855+Kingfinik98@users.noreply.github.com>
2026-03-11 18:40:44 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ if [ "$KVER" == "6.6" ]; then
 elif [ "$KVER" == "6.1" ]; then
   KERNEL_REPO="https://github.com/ramabondanp/android_kernel_common-6.1.git"
   ANYKERNEL_BRANCH="master"
-  KERNEL_BRANCH="android14-6.1"
+  KERNEL_BRANCH="android14-6.1-staging"
 elif [ "$KVER" == "5.10" ]; then
   KERNEL_REPO="https://github.com/ramabondanp/android_kernel_common-5.10.git"
   ANYKERNEL_BRANCH="master"
