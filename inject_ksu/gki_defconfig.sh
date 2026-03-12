5e18bca965422efa9e9a491ac9012c1829fb3ae8
VorteX <151031855+Kingfinik98@users.noreply.github.com>
2026-03-12 16:58:05 +0700

    Update gki_defconfig.sh

--- inject_ksu/gki_defconfig.sh
+++ inject_ksu/gki_defconfig.sh
@@ -76,8 +76,8 @@ fi
 echo "⚙️ Adding Universal Performance Tuning"
 cat >> $DEFCONFIG <<EOF
 # --- Universal Performance Tuning ---
-CONFIG_HZ=500
-CONFIG_HZ_500=y
+CONFIG_HZ=1000
+CONFIG_HZ_1000=y
 CONFIG_TMPFS_XATTR=y
 CONFIG_TMPFS_POSIX_ACL=y
 CONFIG_IP_NF_TARGET_TTL=y
