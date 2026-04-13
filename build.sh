90ce15dfd62ea0a8f423b64f105f49de53d5fc34
VorteX <151031855+Kingfinik98@users.noreply.github.com>
2026-04-13 15:32:19 +0700

    Fix comment formatting in build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
-/*!
- * © 2024-2026 Kingfinik98 (VorteX_E-Sport). All Rights Reserved.
- * Original Author: Kingfinik98
- * Original Repository: https://github.com/Kingfinik98/build-vortex
- */
-
-#!/usr/bin/env bash
+#/*!
+# * © 2024-2026 Kingfinik98 (VorteX_E-Sport). All Rights Reserved.
+# * Original Author: Kingfinik98
+# * Original Repository: https://github.com/Kingfinik98/build-vortex
+# */
+##!/usr/bin/env bash
 
 # Constants
 WORKDIR="$(pwd)"
