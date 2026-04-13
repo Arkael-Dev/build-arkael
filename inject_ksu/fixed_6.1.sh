87b780816ae84b38bd1401a4898f72b83307fc36
Kingfinik98 <kingfinix98@gmail.com>
2026-04-13 14:12:55 +0700

    Security: Remove unauthorized references from core files
    Signed-off-by: Kingfinik98 <kingfinix98@gmail.com>

--- inject_ksu/fixed_6.1.sh
+++ inject_ksu/fixed_6.1.sh
@@ -2,7 +2,6 @@
  * © 2024-2026 Kingfinik98 (VorteX_E-Sport). All Rights Reserved.
  * Original Author: Kingfinik98
  * Original Repository: https://github.com/Kingfinik98/build-vortex
- * Modifying or claiming this as your own (e.g., Generic Zixine) is prohibited.
  */
 
 #!/bin/bash
