db0d8abdb670190ac9ac024aa78cd8c82dedae55
VorteX <151031855+Kingfinik98@users.noreply.github.com>
2026-04-14 17:19:42 +0700

    Fix storage bug by enabling ZRAM writeback and tracking

--- inject_ksu/gki-deconfig-6.1.sh
+++ inject_ksu/gki-deconfig-6.1.sh
@@ -79,9 +79,8 @@ set_config "CONFIG_CPU_FREQ_GOV_ONDEMAND=y"
 set_config "CONFIG_SWAP=y"
 set_config "CONFIG_BLK_DEV_ZRAM=y"
 set_config "CONFIG_ZRAM_DEF_COMP_LZ4=y"
-# FIX STORAGE BUG:
-set_config "CONFIG_ZRAM_WRITEBACK=n"
-set_config "CONFIG_ZRAM_MEMORY_TRACKING=n"
+set_config "CONFIG_ZRAM_WRITEBACK=y"
+set_config "CONFIG_ZRAM_MEMORY_TRACKING=y"
 
 # I/O Scheduler — Kyber for UFS 4.0
 set_config "CONFIG_MQ_IOSCHED_KYBER=y"
