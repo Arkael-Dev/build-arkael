280f34d53e3bd1effb101a0d0cb4c8df64189b9c
VorteX <151031855+Kingfinik98@users.noreply.github.com>
2026-04-14 15:46:20 +0700

    Add CONFIG_CPU_FREQ_GOV_VORTEXCORE setting

--- inject_ksu/gki-deconfig-6.1.sh
+++ inject_ksu/gki-deconfig-6.1.sh
@@ -72,6 +72,7 @@ echo "⚙️ Adding Universal Performance Tuning"
 
 set_config "CONFIG_CPU_FREQ=y"
 set_config "CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y"
+set_config "CONFIG_CPU_FREQ_GOV_VORTEXCORE=y"
 set_config "CONFIG_CPU_FREQ_GOV_ONDEMAND=y"
 
 # Better I/O & filesystem performance
