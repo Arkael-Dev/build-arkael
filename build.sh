121a9d530239c479e05292b39cdc0df6be5bc532
VorteX <151031855+Kingfinik98@users.noreply.github.com>
2026-04-15 19:53:23 +0700

    Update AnyKernel repository URL in build script

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ KERNEL_NAME="VorteX_E-Sport"
 USER="VorteX"
 HOST="VorteX"
 TIMEZONE="Asia/Jakarta"
-ANYKERNEL_REPO="https://github.com/Kingfinik98/AnyKernel3"
+ANYKERNEL_REPO="https://github.com/Kingfinik98/AnyKernel"
 
 # Fixed Logic: 5.10 & 6.1 use gki_defconfig, others use quartix_defconfig
 if [ "$KVER" == "5.10" ]; then
