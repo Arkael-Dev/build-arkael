b0cc8af564b1e2fac5310af9420c0081f0840ad7
VorteX <151031855+Kingfinik98@users.noreply.github.com>
2026-04-19 10:50:20 +0700

    Update kernel repository and branch for version 5.10

--- build.sh
+++ build.sh
@@ -37,9 +37,9 @@ elif [ "$KVER" == "6.1" ]; then
   ANYKERNEL_BRANCH="master"
   KERNEL_BRANCH="android14-6.1-staging"
 elif [ "$KVER" == "5.10" ]; then
-  KERNEL_REPO="https://github.com/ramabondanp/android_kernel_common-5.10.git"
+  KERNEL_REPO="https://github.com/Kingfinik98/kernel-common-android12-5.10.git"
   ANYKERNEL_BRANCH="master"
-  KERNEL_BRANCH="android12-5.10-staging"
+  KERNEL_BRANCH="vortex-basse"
 fi
 DEFCONFIG_TO_MERGE=""
 GKI_RELEASES_REPO="https://github.com/Kingfinik98/build-vortex/releases"
