cf925582d1733286e0d2c87935467ef80adc60ee
Arkaelkernel <151031855+Arkael-Dev@users.noreply.github.com>
2026-05-23 12:09:54 +0700

    Update kernel repository and branch for KVER 5.10

--- build.sh
+++ build.sh
@@ -41,9 +41,9 @@ elif [ "$KVER" == "6.1" ]; then
   ANYKERNEL_BRANCH="master"
   KERNEL_BRANCH="android14-6.1-staging"
 elif [ "$KVER" == "5.10" ]; then
-  KERNEL_REPO="https://github.com/Fleur-Project/android_kernel_xiaomi_sm7435.git"
+  KERNEL_REPO="https://github.com/Arkael-Dev/kernel-common-android12-5.10.git"
   ANYKERNEL_BRANCH="master"
-  KERNEL_BRANCH="droidspaces"
+  KERNEL_BRANCH="android12-5.10-staging"
 fi
 DEFCONFIG_TO_MERGE=""
 GKI_RELEASES_REPO="https://github.com/Arkael-Dev/build-arkael/releases"
